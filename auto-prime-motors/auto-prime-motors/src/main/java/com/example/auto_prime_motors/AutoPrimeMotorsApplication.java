4ab7b5fbfd8fed5aefc46b1ded1474f7bdaa9dbf
guihhramos <guiramos902@gmail.com>
2025-08-27 23:21:09 -0300

    AUTO PRIME MOTORS

---
+++ auto-prime-motors/auto-prime-motors/src/main/java/com/example/auto_prime_motors/AutoPrimeMotorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_prime_motors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPrimeMotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPrimeMotorsApplication.class, args);
+	}
+
+}
